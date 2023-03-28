865b92a70174f4472360988527d3a45eb0d8e0c7
git_Vickyprinz <kariukivictor315@gmail.com>
2023-03-28 20:52:59 +0300

    added items to display in the home and some styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Home.css';
 import Navbar from './Navbar';
 
 const Home = () => {
@@ -20,21 +21,28 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <h1>Products</h1>
-      {products && products.length > 0 ? (
-        products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <button>Add to Cart</button>
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div className="row">
+        {products && products.length > 0 ? (
+          products.map((product) => (
+            <div className="col-lg-4 col-md-6 mb-4" key={product.id}>
+              <div className="card">
+                <img className="card-img-top" src={product.image} alt="product img" />
+                <div className="card-body">
+                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-text">{product.description}</p>
+                  <p className="card-text">${product.price}</p>
+                  <button className="btn btn-primary">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 };
