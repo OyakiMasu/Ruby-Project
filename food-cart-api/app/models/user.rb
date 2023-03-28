eaa1b5911a49b068c2b5f74bb186ec28a8d3c1b8
joy <letimjoy7@gmail.com>
2023-03-28 15:33:30 +0300

    added changes to users

--- food-cart-api/app/models/user.rb
+++ food-cart-api/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :orders dependent: :destroy
+
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEX}
+    validates :username, presence: true, uniqueness: true
+    validates :password, length : {minimum: 6}, if: -> { new_record? || !password.nil?}
+    validates :gender, presence: true
 end
