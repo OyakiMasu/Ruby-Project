082936837f94c11cfd2cbf0bb88441fbfb8a56f0
OyakMasu21 <akzaki17@gmail.com>
2023-03-30 13:43:48 +0300

    Added change_password and a validation for star_rating

--- food-cart-api/app/controllers/users_controller.rb
+++ food-cart-api/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authorize, only: [:show, :update, :destroy]
+    # before_action :authorize, only: [:show, :update, :destroy]
 
 # Handle ActiveRecord Not Found exception
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
@@ -10,7 +10,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
         render json: users, include: :users
     end
 
-    # GET  /users/:id
+    # GET /users/:id
     def show
         user = User.find(session[:user_id])
         render json: user, status: :ok
@@ -46,6 +46,17 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
             head :no_content
         else
             render json: { error: "User not found" }, status: :not_found
+        end
+    end
+
+    # UPDATE /Password/:id
+    def change_password
+        user = User.find(params[:id])
+        if user && user.authenticate(params[:current_password])
+            user.update(password: params[:new_password])
+            render json: { message: 'Password updated succesfully'}, status: :ok
+        else
+            render json: { error: 'Invalid current password' }, status: :unprocessable_entity
         end
     end
 
